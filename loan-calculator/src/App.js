6712495e19cbd927df7635815b42c53e705d7283
Kate Schaffer <krschaffer@lawn-143-215-58-194.lawn.gatech.edu>
2020-08-28 22:43:23 -0400

    input containers for monthly payment calculator

--- loan-calculator/src/App.js
+++ loan-calculator/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Tabs } from 'antd';
+import AffordabilityCalculator from './AffordabilityCalculator';
+import MonthlyPaymentCalculator from './MonthlyPaymentCalculator';
+import CashBackCalculator from './CashBackCalculator';
+
 import './App.css';
 import "antd/dist/antd.css";
 
@@ -8,16 +12,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"/>
-      <div>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Vehicle Affordability" key="1">
-            Content of Tab Pane 1
+      <div className='tabs-container'>
+        <Tabs defaultActiveKey="1" type="card">
+          <TabPane tab="Monthly Payment" key="1">
+            <MonthlyPaymentCalculator />
           </TabPane>
-          <TabPane tab="Monthly Payment" key="2">
-            Content of Tab Pane 2
+          <TabPane tab="Vehicle Affordability" key="2">
+            <AffordabilityCalculator />
           </TabPane>
           <TabPane tab="Low Rate vs. Cash Back" key="3">
-            Content of Tab Pane 3
+            <CashBackCalculator />
           </TabPane>
         </Tabs>
       </div>
--- loan-calculator/src/MonthlyPaymentCalculator.js
+++ loan-calculator/src/MonthlyPaymentCalculator.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Collapse } from 'antd';
+import './MonthlyPaymentCalculator.css';
 
 function MonthlyPaymentCalculator() {
+  const { Panel } = Collapse;
+
   return (
     <div>
-      <p>Monthly Payment Calculator</p>
+      <div className='calculator-inputs'>
+        <Collapse bordered={false} defaultActiveKey={['1', '2', '3']}>
+          <Panel header="Vehicle Information" key="1">
+            vehicle purchase price, cash back, sales tax rate
+          </Panel>
+          <Panel header="Trade-In Information" key="2">
+            value of trade-in, amount owed on trade-in
+          </Panel>
+          <Panel header="Loan Information" key="3">
+            loan terms (months), interest rate, down payment
+          </Panel>
+        </Collapse>
+      </div>
     </div>
   );
 }
