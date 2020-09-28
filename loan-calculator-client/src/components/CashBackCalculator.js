dbe3a58bb879033a43be9c3343dada6d71c56728
Yasmine Soubai <yasminesoubai99@gmail.com>
2020-09-28 19:24:51 -0400

    Added inputs

--- loan-calculator-client/src/components/CashBackCalculator.js
+++ loan-calculator-client/src/components/CashBackCalculator.js
@@ -1,9 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Form,
+  InputNumber,
+  Radio
+} from 'antd';
+import { calculateCashBack } from '../scripts/calculators';
+import '../css/AffordabilityCalculator.css';
+import BarChart from './BarChart';
 
 function CashBackCalculator() {
+  // create the inputs for our calculator
+  const [ inputs, setInputs ] = useState({
+    purchasePrice: 0,
+    cashBack: 0,
+    lowinterestrate: 0,
+    taxRate: 0,
+    tradeInValue: 0,
+    tradeInOwed: 0,
+    loanTerm: 0,
+    interestRate: 0,
+    downPayment: 0
+  });
+  
+    // make the panels collapsed
+    const { Panel } = Collapse;
+
   return (
     <div>
-      <p>Cash Back Calculator</p>
+      <div className='calculator-inputs'>
+        <Collapse bordered={false} defaultActiveKey={['1', '2', '3']}>
+        <Panel header="Vehicle Information" key="1">
+            <Form
+              name="cashback-inputs"
+              fields={[
+                { name: ["purchasePrice"], value: inputs.purchasePrice },
+                { name: ["cashBack"], value: inputs.cashBack },
+                { name: ["lowinterestrate"], value: inputs.lowinterestrate },
+                { name: ["taxRate"], value: inputs.taxRate }
+              ]}
+              onFieldsChange={(changedFields, allFields) => {
+                //onInputsChange(changedFields);
+              }}
+            >
+              <Form.Item label="Vehicle Purchase Price">
+                <Form.Item name="purchasePrice" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Cash Back">
+                <Form.Item name="cashBack" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Low Interest Rate">
+                <Form.Item name="lowinterestrate" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Sales Tax Rate">
+                <Form.Item name="taxRate" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+            </Form>
+          </Panel>
+          <Panel header="Trade-In Information" key="2">
+          <Form
+              name="monthly-payment-inputs"
+              fields={[
+                { name: ["tradeInValue"], value: inputs.tradeInValue },
+                { name: ["tradeInOwed"], value: inputs.tradeInOwed }
+              ]}
+              onFieldsChange={(changedFields, allFields) => {
+                //onInputsChange(changedFields);
+              }}
+            >
+              <Form.Item label="Value of Trade-in">
+                <Form.Item name="tradeInValue" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Amount Owed on Trade-in">
+                <Form.Item name="tradeInOwed" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+            </Form>
+          </Panel>
+          <Panel header="Loan Information" key="3">
+            <Form
+              name="monthly-payment-inputs"
+              fields={[
+                { name: ["loanTerm"], value: inputs.loanTerm },
+                { name: ["interestRate"], value: inputs.interestRate },
+                { name: ["downPayment"], value: inputs.downPayment }
+              ]}
+              onFieldsChange={(changedFields, allFields) => {
+                //onInputsChange(changedFields);
+              }}
+            >
+              <Form.Item label="Loan Term (months)">
+                <Form.Item name="loanTerm" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Interest Rate">
+                <Form.Item name="interestRate" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item label="Down Payment">
+                <Form.Item name="downPayment" noStyle>
+                  <InputNumber min={0} />
+                </Form.Item>
+              </Form.Item>
+            </Form>
+          </Panel>
+        </Collapse>
+      </div>
     </div>
   );
 }
